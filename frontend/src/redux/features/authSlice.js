01a51dff95759a694a488dec63a28755b54e3b50
Abhimanyu Singh <127104226+abhimanyuspark@users.noreply.github.com>
2026-03-24 22:20:13 +0530

    dfsd

--- frontend/src/redux/features/authSlice.js
+++ frontend/src/redux/features/authSlice.js
@@ -27,12 +27,8 @@ export const loginUser = createAsyncThunk(
       const response = await api.post(`/auth/login`, userData);
       return response.data;
     } catch (err) {
-      const message =
-        err?.response?.data?.message ||
-        err?.response?.data ||
-        err?.message ||
-        "Login failed. Please try again.";
-      return rejectWithValue(message);
+      console.log(err)
+      return rejectWithValue(err.response?.data?.message || err.message);
     }
   },
 );
